849cefcd1a3b783cc22370c6cd2f867c66a53fed
azu <azuciao@gmail.com>
2021-10-24 14:45:26 +0900

    initial commit

---
+++ src/textlint-rule-gramma.ts
@@ -0,0 +1,40 @@
+import { TextlintRuleReporter } from "@textlint/types";
+import { StringSource } from "textlint-util-to-string";
+import gramma from "gramma";
+
+export type Options = {
+    api_url?: string;
+    api_key?: string;
+    language?: string;
+    rules?: { [ruleName: string]: boolean };
+    dictionary?: string[];
+    markdown?: boolean;
+};
+const report: TextlintRuleReporter<Options> = (context, options) => {
+    const { Syntax, report, RuleError, fixer } = context;
+    return {
+        async [Syntax.Paragraph](node) {
+            const source = new StringSource(node);
+            const { matches } = await gramma.check(source.toString(), options);
+            matches.forEach((match) => {
+                const originalStatIndex = source.originalIndexFromIndex(match.offset);
+                const originalEndIndex = source.originalIndexFromIndex(match.offset + match.length);
+                const correctLine = originalStatIndex !== undefined && originalEndIndex !== undefined;
+                report(
+                    node,
+                    new RuleError(match.message, {
+                        index: originalEndIndex,
+                        fix: correctLine
+                            ? // FIXME: should be suggest
+                              fixer.replaceTextRange([originalStatIndex, originalEndIndex], match.replacements[0].value)
+                            : undefined
+                    })
+                );
+            });
+        }
+    };
+};
+export default {
+    linter: report,
+    fixer: report
+};
---
+++ test/textlint-rule-gramma.test.ts
@@ -0,0 +1,47 @@
+import TextLintTester from "textlint-tester";
+// a rule for testing
+import rule, { Options } from "../src/textlint-rule-gramma";
+
+const tester = new TextLintTester();
+const testOptions: Options = {
+    api_url: "http://localhost:8081/v2/check"
+};
+tester.run("textlint-rule-gramma", rule, {
+    valid: [
+        "This is ok",
+        {
+            text: "It is testing on local server",
+            options: testOptions
+        }
+    ],
+    invalid: [
+        {
+            text: "Suggestions for this sentences will be printed.",
+            output: "Suggestions for these sentences will be printed.",
+            errors: [
+                {
+                    message:
+                        "The demonstrative ‘this’ may not agree with the plural noun ‘sentences’. Did you mean “these”?",
+                    index: 20
+                }
+            ]
+        },
+        {
+            text: "It are wrong.\n\nIt are good.",
+            output: "It is wrong.\n\nIt is good.",
+            errors: [
+                {
+                    message: "After ‘It’, use the third-person verb form “is”.",
+                    line: 1,
+                    column: 7
+                },
+                {
+                    message: "After ‘It’, use the third-person verb form “is”.",
+                    line: 3,
+                    column: 7
+                }
+            ],
+            options: testOptions
+        }
+    ]
+});
